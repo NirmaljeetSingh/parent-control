d9bc940d325577ca52fefc084a79bea942a4b9a8
Nirmaljeet <nirmaljeets20@gmail.com>
2023-06-28 22:44:59 +0530

    duplicate number removed

--- app/Http/Controllers/API/ContactFinderController.php
+++ app/Http/Controllers/API/ContactFinderController.php
@@ -62,27 +62,31 @@ class ContactFinderController extends Controller
         if($er = __validation($request->all(),$rules)) return $er;
         $app_exists = [];
         $app_not_exists = [];
+        $phone_numbers = [];
+        
         foreach($request->data as $val){
             $hiphen_replace = str_replace("-","",$val['number']);
             $white_space = str_replace(" ","",$hiphen_replace);
             $braces = str_replace(")","",str_replace("(","",$white_space));
             $phone = str_replace('+','',$braces);
             $phone_numbers[] = $phone;
-            $temp_obj = ['firstName' => $val['firstName'] ?? '','number' => $val['number'] ?? '','lastName' => $val['lastName'] ?? '','user' => []];
-            try {
-                //code...
-                if($user = User::whereRaw("CONCAT(code,'',phone_no) LIKE '%{$phone}'")->first())
-                {
-                    $temp_obj['user'] = $user;
-                    $app_exists[] = $temp_obj;
+            if(!in_array($phone,$phone_numbers)){
+                $temp_obj = ['firstName' => $val['firstName'] ?? '','number' => $val['number'] ?? '','lastName' => $val['lastName'] ?? '','user' => []];
+                try {
+                    //code...
+                    if($user = User::whereRaw("CONCAT(code,'',phone_no) LIKE '%{$phone}'")->first())
+                    {
+                        $temp_obj['user'] = $user;
+                        $app_exists[] = $temp_obj;
+                    }
+                    else
+                    {
+                        $app_not_exists[] = $temp_obj;
+                    }
+                } catch (\Throwable $th) {
+                    // return  $th->getMessage();
+                    // return User::whereRaw("CONCAT(code,'',phone_no) LIKE %{$phone}")->toSql();
                 }
-                else
-                {
-                    $app_not_exists[] = $temp_obj;
-                }
-            } catch (\Throwable $th) {
-                // return  $th->getMessage();
-                // return User::whereRaw("CONCAT(code,'',phone_no) LIKE %{$phone}")->toSql();
             }
         }
         return success_response(['has_app' => $app_exists,'not_has_app' => $app_not_exists],'List fetch successfully.');
